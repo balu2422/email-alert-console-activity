cbe33f28cda1c09f41a4dce02fb08be246959610
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-19 13:57:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   backend "s3" {
-    bucket  = "balastatefilebucket "
+    bucket  = "balastatefilebucket"
     key     = "tf/notify"
     region  = "us-east-1"
     encrypt = true
