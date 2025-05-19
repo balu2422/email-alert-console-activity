7ccbe4e4031847b64df51d300906cbc7f6bd6ee3
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-19 14:21:54 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -11,6 +11,38 @@ resource "aws_s3_bucket" "trail_bucket" {
   }
 }
 
+# Add the required S3 bucket policy for CloudTrail to write logs
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket_policy" "trail_bucket_policy" {
+  bucket = aws_s3_bucket.trail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailAclCheck20150319"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:GetBucketAcl"
+        Resource  = aws_s3_bucket.trail_bucket.arn
+      },
+      {
+        Sid       = "AWSCloudTrailWrite20150319"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "s3:PutObject"
+        Resource  = "${aws_s3_bucket.trail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_cloudwatch_log_group" "trail" {
   name              = "/aws/cloudtrail/activity"
   retention_in_days = 7
@@ -56,11 +88,12 @@ resource "aws_cloudtrail" "trail" {
   enable_log_file_validation    = true
   enable_logging                = true
   cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_role.arn
-  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.trail.arn}:*"
+  cloud_watch_logs_group_arn    = aws_cloudwatch_log_group.trail.arn
 
   depends_on = [
     aws_cloudwatch_log_group.trail,
-    aws_iam_role_policy.cloudtrail_policy
+    aws_iam_role_policy.cloudtrail_policy,
+    aws_s3_bucket_policy.trail_bucket_policy
   ]
 }
 
