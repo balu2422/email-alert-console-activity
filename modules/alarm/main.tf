eb1e552d4f3136a0d151c99b4fb2e801f5c2475a
balu2422 <157208069+balu2422@users.noreply.github.com>
2025-05-19 20:19:53 +0530

    Update main.tf

--- modules/alarm/main.tf
+++ modules/alarm/main.tf
@@ -1,13 +1,3 @@
-variable "log_group_name" {
-  type = string
-}
-variable "filter_pattern" {
-  type = string
-}
-variable "sns_topic_arn" {
-  type = string
-}
-
 resource "aws_cloudwatch_metric_alarm" "login_alarm" {
   alarm_name          = "ConsoleLoginAlarm"
   alarm_description   = "Triggers on AWS Console login attempts"
